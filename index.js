c9fddb5273d29ec402757cc6fb3d94998501b29a
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-05-15 19:57:16 -0300

    Aula 01 DH

---
+++ index.js
@@ -0,0 +1,11 @@
+//Esse aqui é um comentário
+
+
+var minhaVariavel = "oi";
+console.log(minhaVariavel)
+
+let minhaVariavel1 = "tudo bem";
+console.log(minhaVariavel);
+
+const minhaVariavel2 = "aula01";
+console.log(minhaVariavel2);
