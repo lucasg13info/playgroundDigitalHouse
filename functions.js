ce49ce8e0a4261d53d6222323fd5ef9295054ad2
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-05-21 20:06:18 -0300

    Validações IF

--- functions.js
+++ functions.js
@@ -1,28 +1,67 @@
-//Modo declarativo
+// //Modo declarativo
 
-function somar(a,b){
-    //cod
-    console.log("estou somando")
-    return a + b;
+// function somar(a,b){
+//     //cod
+//     console.log("estou somando")
+//     return a + b;
+// }
+// let resultadoSoma = somar (1,2);
+// console.log(resultadoSoma);
+
+
+
+
+// //Modo expressão
+// let multiplicar = function(a,b){
+//     console.log("Estou multiplicando")
+//     return a * b;
+// }
+
+// let resultadoMultiplicar = multiplicar(2,10);
+// console.log(resultadoMultiplicar);
+
+
+
+// function saudar (nome, sobrenome ){
+//     return 'olá, ' + nome + ' ' + sobrenome + '!';
+// }
+// saudar("Lucas", "Estefano");
+
+
+
+
+
+
+//Exercício Playground
+/* Escreva as três funções a seguir:
+
+anterior: recebe um número como parâmetro e retorna esse número menos um.
+triplo: recibe um número como parâmetro e devolve o triplo desse número.
+anteriorDoTriplo: receba um número como parâmetro e, usando as duas funções anteriores, você terá que retornar o número. recebido multiplicado por 3 (três) e o resultado subtrai 1 (um).
+*/
+
+
+function anterior(num){
+    let anteriorA = num -1;
+    return anteriorA;
 }
-let resultadoSoma = somar (1,2);
-console.log(resultadoSoma);
+
+  function triplo(num1){
+    let triploA = num1 * 3; 
+      return triploA;
+  }
+
+  function anteriorDoTriplo(num2){
+    let anteriorDoTriploA = num2
+      return anteriorDoTriploA;
+  }
 
 
+ let resultadoAnterior = anterior(10);
+    console.log(resultadoAnterior);
 
+ let resultadoTriplo = triplo(10);
+     console.log(resultadoTriplo);
 
-//Modo expressão
-let multiplicar = function(a,b){
-    console.log("Estou multiplicando")
-    return a * b;
-}
-
-let resultadoMultiplicar = multiplicar(2,10);
-console.log(resultadoMultiplicar);
-
-
-
-function saudar (nome, sobrenome ){
-    return 'olá, ' + nome + ' ' + sobrenome + '!';
-}
-saudar("Lucas", "Estefano");
+ let resultadoAnteriorTriplo = anteriorDoTriplo(10);
+     console.log(resultadoAnteriorTriplo *3 - 1);
---
+++ validacoes-if.js
@@ -0,0 +1,8 @@
+let dia = "domingo";
+
+if (dia == "domingo"){
+    console.log(" Vou para a praia!")
+
+}else{
+    console.log("Outro dia")
+}
