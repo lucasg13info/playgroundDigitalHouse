371449daba44db64df6c2788d7cc613739dc56a4
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-05-21 20:34:58 -0300

    Validações IF els IF

--- validacoes-if.js
+++ validacoes-if.js
@@ -1,8 +1,32 @@
-let dia = "domingo";
+let dia = "sexta";
 
 if (dia == "domingo"){
     console.log(" Vou para a praia!")
 
+}else if (dia == "sexta"){
+    console.log("Vou ao cinema")
 }else{
-    console.log("Outro dia")
+    console.log("Vou dormir")
 }
+
+
+let linguagem = "javascript"
+
+if (linguagem === "javascript"){
+    console.log("Estou aprendendo")
+}else{
+    console.log("Vou aprender mais pra frente")
+}
+
+
+
+function podePassar(nome){
+    if (nome === "Natalia") {
+        return false;
+    }else if (nome !== "Natalia"){
+        return true;
+    }
+    
+}
+
+podePassar("Natalia");
