eec4ac3ea77d47b3322cb8d5c3b86806827f258e
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-05-21 23:16:47 -0300

    Convertendo dados

--- arrow.js
+++ arrow.js
@@ -1,3 +1,10 @@
-const somar = numero => console.log("Recebi um número " + " " + numero)
+const somar1 = (numeroA, numeroB) => console.log("Recebi um número " + " " + numeroA)
 
-somar(10);
+somar1(10), 20;
+
+
+const somar = (numeroA, numeroB) => {
+        console.log("Recebi um número " + " " + numeroA)
+        return numeroA + numeroB
+}
+console.log(somar(10), 20);
---
+++ convertendo-dados.js
@@ -0,0 +1,12 @@
+let texto = "5"
+let numero = parseInt(texto)
+
+console.log(typeof(numero))
+
+
+
+
+let num = 111;
+let text = String(num);
+
+console.log(typeof(texto))
