a339334c89948045f7eb30bd2d54a51b04651a5a
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-05-23 13:14:21 -0300

    Exercícios

--- string.js
+++ string.js
@@ -1,19 +1,43 @@
-//lenght - Quantidade de strings, conta também os espaços
-let texto = "Oi eu sou o Lucas"
-console.log(texto.length);
+// //lenght - Quantidade de strings, conta também os espaços
+// let texto = "Oi eu sou o Lucas"
+// console.log(texto.length);
 
 
-//indexOf (se retornar valor negativo significa que não existe a palavra ou letra)
-console.log(texto.indexOf("s"))
+// //indexOf (se retornar valor negativo significa que não existe a palavra ou letra)
+// console.log(texto.indexOf("s"))
 
 
-//slice - Pega uma fatia da informação
-console.log(texto.slice (0,3))
+// //slice - Pega uma fatia da informação
+// console.log(texto.slice (0,3))
 
 
-//trim - remove espaços em branco no início e no fim 
-console.log(texto.trim())
+// //trim - remove espaços em branco no início e no fim 
+// console.log(texto.trim())
 
 
-//slip - tranforma a string em um array
-console.log(texto.split(" "))
+// //slip - tranforma a string em um array
+// console.log(texto.split(" "))
+
+// //replace - trocar nomes
+// let textoNovo = texto.replace("Oi", "olá")
+// console.log(textoNovo)
+
+
+
+function dominio(textoOld, procurar, troca){
+    return textoOld.replace(procurar, troca)
+
+}
+let novo = dominio ("br.digitalhouse.com" ,"br.digitalhouse.com", "http://br.digitalhouse.com") 
+console.log (novo);
+
+
+
+
+
+// function substituicaoRapida(texto, procurar, substituir){
+//     return texto.replace(procurar, substituir);
+//   }
+  
+//   let textoNovo = substituicaoRapida('Olá tudo bem','bem','mal');
+//   console.log(textoNovo) //'Boa noite'
