bc65525895ec21a16a0c1337210d5da2a46b7dac
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-05-23 12:20:57 -0300

    sting

---
+++ string.js
@@ -0,0 +1,19 @@
+//lenght - Quantidade de strings, conta também os espaços
+let texto = "Oi eu sou o Lucas"
+console.log(texto.length);
+
+
+//indexOf (se retornar valor negativo significa que não existe a palavra ou letra)
+console.log(texto.indexOf("s"))
+
+
+//slice - Pega uma fatia da informação
+console.log(texto.slice (0,3))
+
+
+//trim - remove espaços em branco no início e no fim 
+console.log(texto.trim())
+
+
+//slip - tranforma a string em um array
+console.log(texto.split(" "))
