36ed05554256af7e085dc06ea200db211b6ce700
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-05-23 13:22:40 -0300

    exercicios

--- string.js
+++ string.js
@@ -1,29 +1,33 @@
-// //lenght - Quantidade de strings, conta também os espaços
-// let texto = "Oi eu sou o Lucas"
-// console.log(texto.length);
+//lenght - Quantidade de strings, conta também os espaços
+let texto = "Oi eu sou o Lucas"
+console.log(texto.length);
 
 
-// //indexOf (se retornar valor negativo significa que não existe a palavra ou letra)
-// console.log(texto.indexOf("s"))
+//indexOf (se retornar valor negativo significa que não existe a palavra ou letra)
+console.log(texto.indexOf("s"))
 
 
-// //slice - Pega uma fatia da informação
-// console.log(texto.slice (0,3))
+//slice - Pega uma fatia da informação
+console.log(texto.slice (0,3))
 
 
-// //trim - remove espaços em branco no início e no fim 
-// console.log(texto.trim())
+//trim - remove espaços em branco no início e no fim 
+console.log(texto.trim())
 
 
-// //slip - tranforma a string em um array
-// console.log(texto.split(" "))
+//slip - tranforma a string em um array
+console.log(texto.split(" "))
 
-// //replace - trocar nomes
-// let textoNovo = texto.replace("Oi", "olá")
-// console.log(textoNovo)
+//replace - trocar nomes
+let textoNovo = texto.replace("Oi", "olá")
+console.log(textoNovo)
 
 
 
+
+//EXERCICIOS:
+
+//Criar uma função chamada dominio que receberá uma String  "br.digitalhouse.com" e sua função será devolver "http://br.digitalhouse.com".
 function dominio(textoOld, procurar, troca){
     return textoOld.replace(procurar, troca)
 
@@ -33,11 +37,20 @@ console.log (novo);
 
 
 
-
-
-// function substituicaoRapida(texto, procurar, substituir){
-//     return texto.replace(procurar, substituir);
-//   }
+// Sua missão agora é criar uma função chamada "substituicaoRapida" que você recebe um texto, uma palavra para procurar e a palavra que substituirá a que você está procurando. A função deve retornar o texto com a palavra substituída.
+function substituicaoRapida(texto, procurar, substituir){
+    return texto.replace(procurar, substituir);
+  }
   
-//   let textoNovo = substituicaoRapida('Olá tudo bem','bem','mal');
-//   console.log(textoNovo) //'Boa noite'
+  let textoNovo = substituicaoRapida('Olá tudo bem','bem','mal');
+  console.log(textoNovo) //'Boa noite'
+
+
+
+
+
+// Declare a string "Olá, sou Carlos!" e depois recorte a mesma para poder obter o nome "Carlos" em uma nova variável chamada nomeUsuario. IMPORTANTE: Para a resolução funcionar, lembre-se de usar a slice().
+let apresentacao ="Olá, sou o Carlos!"
+let nomeUsuario = apresentacao.slice(11, 17)
+
+console.log(nomeUsuario)
