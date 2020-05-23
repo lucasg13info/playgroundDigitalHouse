88973216efc609e2850c617ff44dfc15ccfa287f
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-05-23 16:00:16 -0300

    Assincronismo

---
+++ assincronismo.js
@@ -0,0 +1,17 @@
+//Define o tempo que irá executar cada parte do cod, usa o setTimeout para isso
+
+function alarme1(){
+    return "Hora de acordar são 08:00h";
+}
+
+function alarme2(){
+    return "Está perto da sua hora de acordar são 7:50h"
+
+}
+//Só executa depois de 2s
+setTimeout(function(){
+    console.log(alarme1())
+}, 5000)
+
+//Executa antes
+console.log(alarme2())
