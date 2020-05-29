4e40b285d4a9f8213c25e428ef85e8044ebbad9d
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-05-29 20:00:32 -0300

    Metodos de array

---
+++ Aula3/array.js
@@ -0,0 +1,7 @@
+//MAP - Percorre o array como se fosse um for  e pode alterar posições do array 
+
+let filmes = ["Start wars", "Guerra nas estrelas", "Star trek", "Bea"]
+
+filmes.map(function(item){
+    console.log(item)
+})
