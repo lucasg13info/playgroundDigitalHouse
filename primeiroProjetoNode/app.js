eadb173ae701ab76ee07c3567680a9817de7dd44
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-10 00:01:44 -0300

    Criado servidor para response porém sem sucesso, necessita de ajustes

--- primeiroProjetoNode/app.js
+++ primeiroProjetoNode/app.js
@@ -1,7 +1,18 @@
+// Sevridor que apenas recebe a solicitação  (recebe o requests)
+// const http = require('http');
+
+// http.createServer((req, res) => {
+    
+//     console.log("Servidor está rodando ...")
+
+// }) .listen(3000);
+
+
+//Servidor que recebe o request e retorna o response
 const http = require('http');
 
-http.createServer((req, res) => {
-    
-    console.log("Servidor está rodando ...")
+http.createServer((req, res)=>{
+    res.writeHead(200, {"Content-Type":"test/plain"});
+    res.end("Meu primeiro servidor!");
 
 }) .listen(3000);
