2495d1f6dbde50697be3ed36a846920956c9e06f
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-09 22:20:00 -0300

    Ajustando comentários

--- primeiroProjetoNode/index.js
+++ primeiroProjetoNode/index.js
@@ -1,4 +1,4 @@
-//Importando módulos
+//Importando módulos 1 do próprio node, 2 externa (que instalamos) e 3 que nós criamos. 
 const fs = require('fs');
 let moment = require('moment');
 const superHerois = require('./superHerois/')
@@ -7,6 +7,7 @@ const superHerois = require('./superHerois/')
 //Executando os módulos 
 let dados = fs.readFileSync(__dirname + '/dados.txt', 'utf8') //___dirname = nome do arquivo
 let data = moment().format('MMM do YY') //() serve para executar o moment
+
 console.log(data)
 
 
