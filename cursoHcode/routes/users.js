dbc2839cabd0a46f17ff00e3e4d2d4e56e29b9c1
Lucas Rodrigues Estefano <lucccasestefano1@gmail.com>
2020-06-16 21:37:12 -0300

    Estudando com Hcode, criando servidor com express, nodemon, consign e NeDb

---
+++ cursoHcode/routes/users.js
@@ -0,0 +1,41 @@
+let Nedb =require( 'nedb' )
+let db = new Nedb({
+    filename:'users.db',
+    autoload:true
+})
+
+module.exports = (app)=>{
+
+    app.get('/users', (req, res) => {
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+                users:[{
+                    name: 'Hcode',
+                    email: 'contato@hcode.com.br',
+                    id: 1
+                }]
+        });
+    });
+    
+    
+    app.post('/users/', (req, res) =>{
+        
+        db.insert(req.body, (err, user)=>{
+            
+            if (err){
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error:err
+                })
+            }else{
+                res.status(200).json(user);
+            }
+        });
+    });
+    
+};
+
+
+
