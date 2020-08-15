7cc0ea8666ece52b577e3eb4f33514d577a34797
Lucas Rodrigues Estefano <lucccasestefano1@gmil.com>
2020-08-15 15:38:27 -0300

    Controllers

--- Back-end/estudoExpress/Routes/produtos.js
+++ Back-end/estudoExpress/Routes/produtos.js
@@ -4,5 +4,6 @@ const ProdutoController = require ('../controllers/ProdutoController')
 
 
 router.get('/produto', ProdutoController.criarProduto)
+router.get('/deletar/:id', ProdutoController.deletarProduto)
 
 module.exports = router;
--- Back-end/estudoExpress/controllers/ProdutoController.js
+++ Back-end/estudoExpress/controllers/ProdutoController.js
@@ -1,6 +1,11 @@
 const ProdutoController = {
     criarProduto: (req, res)=>{
         res.send("Criando um prduto");
+    },
+    deletarProduto:(req, res) =>{
+        const {id} = req.params;
+
+        res.send ("Deletando o produto com id: " + id);
     }
 }
 
